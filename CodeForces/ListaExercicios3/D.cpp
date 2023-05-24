f557a10240d9e4f720874a44933b124fc4769584
thiagorigstak <thiagorigstak@gmail.com>
2023-05-24 20:26:23 -0300

    ListaExercicios3

--- CodeForces/ListaExercicios3/D.cpp
+++ CodeForces/ListaExercicios3/D.cpp
@@ -6,8 +6,24 @@ using namespace std;
 //typedef long long int ll; define ll para long long int "macro"
 int main(){
     desync;
-    // 1LL auto cast de int diretamente para long long
-    //code goes here
+    int sum = 0, oldsum = 0;
+    int x = 3, k,j;
+    cin >> k;
+    while(x){
+        if(x < 3){
+            oldsum = sum;
+            sum = 0;
+        }
+        for(int i = 0; i < k; i++){
+            cin >> j;
+            sum += j;
+        }
+        if(x < 3){
+            cout << abs(sum - oldsum) << endl;
+        }
+        k--;
+        x--;
+    }
     return 0;
 }
 // notes : endl mais devagar que \n
--- CodeForces/ListaExercicios3/E.cpp
+++ CodeForces/ListaExercicios3/E.cpp
@@ -6,8 +6,37 @@ using namespace std;
 //typedef long long int ll; define ll para long long int "macro"
 int main(){
     desync;
-    // 1LL auto cast de int diretamente para long long
-    //code goes here
+    int n, k1,k2,x, res = 0, k;
+    vector<int> dist;
+    priority_queue<int> pq;
+    cin >> n >> k1 >> k2;
+    k = k1+k2;
+    for(int i = 0;i < n; i++){
+        cin >> x;
+        dist.push_back(x);
+    }
+    for(int i = 0;i < n; i++){
+        cin >> x;
+        x = abs(x);
+        pq.push(abs(dist[i] - x));
+    }
+    while(k--){
+        x = pq.top();
+        if(x == 0){
+            x++;
+            pq.pop();
+            pq.push(x);
+        }else{
+            x--;
+            pq.pop();
+            pq.push(x);
+        }
+    }
+    while(!pq.empty()){
+        res += pq.top() * pq.top();
+        pq.pop();
+    }
+    cout << res << endl;
     return 0;
 }
 // notes : endl mais devagar que \n
@@ -15,3 +44,4 @@ int main(){
 // continue; - pula o bloco de comando atual e vai pro prox
 // break; - sai do laço definitivamente
 // min element vetor ou max element vetor ==> auto min = min_element(temp.begin(),temp.end()); cout << *min << endl; min é um ponteiro pro menor elemento do vetor
+// sort(me.begin(),me.end(),greater<int>()); // macete de ordenar inverso
