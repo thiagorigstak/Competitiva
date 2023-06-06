1c6c2d6352e67ecc1e31232cd536ba58ce6901a3
thiagorigstak <thiagorigstak@gmail.com>
2023-06-06 17:26:03 -0300

    EL4

--- CodeForces/ListaExercicios4/E.cpp
+++ CodeForces/ListaExercicios4/E.cpp
@@ -6,18 +6,37 @@ using namespace std;
 //typedef long long int ll; define ll para long long int "macro"
 int main(){
     desync;
-    ll n,q,l,r,x, aux = 0,psum, msum;
-    vector<ll> arr;
+    ll n,q,l,r,x, aux = 0, maior = -1,sol = 0;
     cin >> n >> q;
+    priority_queue<ll> arr;
+    vector<ll> delta(n+2,0),psum(n+1,0);
     for(int i = 0; i < n; i++){
         cin >> x;
-        aux += x;
-        arr.push_back(aux);
+        arr.push(x);
     }
     for(int i = 0; i < q; i++){
         cin >> l >> r;
+        delta[l] += 1;
+        delta[r + 1] -= 1;
     }
-    cout << msum << endl;
+    for(int i = 1; i <= n; i++){
+        aux += delta[i];
+        if(aux > maior){
+            maior = aux;
+        }
+        psum[i] = aux;
+        //cout << psum[i] << ' ';
+    }
+    while(!arr.empty()){
+        for(int j = 1; j <= n ; j++){
+            if(psum[j] == maior){
+                sol += psum[j] * arr.top();
+                arr.pop();
+            }
+        }
+        maior--;
+    }
+    cout << sol << endl;
     return 0;
 }
 // notes : endl mais devagar que \n
